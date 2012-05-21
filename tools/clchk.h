990501f838079efa27ab395a9f24046a59fb646c
Andrew J. Musselman <ki6uoc@gmail.com>
2012-05-20 20:22:50 -0700

    Added a simple OpenCL source checker

---
+++ tools/clchk.h
@@ -0,0 +1,21 @@
+/**
+ * A program to attempt to compile open cl code
+ *
+ * Code borrowed from http://www.drdobbs.com/parallel/231002854
+ */
+#ifndef clchk_h
+#define clchk_h
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#ifdef MAC
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#endif
