34ece640c50804f6d52b62eb7075caee2cec0fb7
Andrew J. Musselman <ki6uoc@gmail.com>
2013-04-22 18:58:27 -0700

    Got the Gprof Parsing working... Need to come up with a format for the output

--- documents/parse_gprof_restults.pl
+++ documents/parse_gprof_restults.pl
@@ -6,29 +6,68 @@
 use strict;
 use Data::Dumper;
 
+sub parseCallLine{
+   my $line = $_[0];
+   my $output;
+   my @splitLine = split(/\s+/,$line);
+   my $name;
+   my $nameIdxStart;
+   
+   $output->{'timePercent'}=$splitLine[1]; 
+   $output->{'timeCumSeconds'}=$splitLine[2]; 
+   $output->{'timeSelfSeconds'}=$splitLine[3]; 
+   if($splitLine[4] =~ /^\d/){
+      $output->{'calls'}=$splitLine[4];
+      $output->{'selfcallssecond'}=$splitLine[5];
+      $output->{'totalcallssecond'}=$splitLine[6];
+      $nameIdxStart = 7;
+   }
+   else{
+      $nameIdxStart = 4;
+   }
+   $name = join(' ',@splitLine[$nameIdxStart..$#splitLine]);
+   $output->{'name'}=$name; 
+
+   return $output;
+}
+
 my $line;
 my $dataHash;
 my $parseCallProfile;
+my $mile;
+my $results;
+my $maxMile = 0;
 
 
 while($line= <STDIN>){
    chomp($line);
    if($line =~ /^Run/){
       if( $line =~ s/\D//g ){
-            $dataHash->{'mile'} = $line;
+            $mile = $line;
       }
       
    } 
-   if($line =~ /^ time/){
-      $parseCallProfile = 1;
-   }
-   if($parseCallProfile == 1){
-      print($line."\n");
-   }
+
    if($line =~ /^ %/){
       $parseCallProfile = 0;
    }
+   if($parseCallProfile == 1){
+      $dataHash = parseCallLine($line);
+      $results->{$dataHash->{'name'}}[$mile]->{'selfcallssecond'}=$dataHash->{'selfcallssecond'};
+      $results->{$dataHash->{'name'}}[$mile]->{'calls'}=$dataHash->{'calls'};
+      $results->{$dataHash->{'name'}}[$mile]->{'timeCumSeconds'}=$dataHash->{'timeCumSeconds'};
+      $results->{$dataHash->{'name'}}[$mile]->{'totalcallssecond'}=$dataHash->{'totalcallssecond'};
+      $results->{$dataHash->{'name'}}[$mile]->{'timePercent'}=$dataHash->{'timePercent'};
+      $results->{$dataHash->{'name'}}[$mile]->{'timeSelfSeconds'}=$dataHash->{'timeSelfSeconds'};
+   }
+   if($line =~ /^ time/){
+      $parseCallProfile = 1;
+   }
 
-
+   if($mile > $maxMile){
+      $maxMile = $mile;
+   }
 }
-print Dumper($dataHash);
+
+
+print Dumper($results);
