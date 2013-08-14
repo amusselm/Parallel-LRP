704855843e4a77abecceca1e57807e0ebc9fb00d
Andrew J. Musselman <ki6uoc@gmail.com>
2013-08-14 16:02:41 -0700

    Do things one mile at a time, fixed typos

--- documents/distance_benchmark3.sh
+++ documents/distance_benchmark3.sh
@@ -31,8 +31,8 @@ then
 fi
 
 output_dir=$test_dir/opencl
-for((i = 10; i<= $max_distance; i = i+10)) do
-   echo "Running $i miles of $max_distances in OpenCL"
+for((i = 1; i<= $max_distance; i = i+1)) do
+   echo "Running $i miles of $max_distance in OpenCL"
    echo "Run with $i points:" > $output_dir/time_$i.out
    (time ../psplat/psplat -d ../../splat/eledata/sdf/ -L 10 -R $i -t W6BHZ.qth) 1>$output_dir/stdout_$i.out 2>> $output_dir/time_$i.out
    echo "Run with $i miles:" > $output_dir/gprof_$i.out
@@ -40,8 +40,8 @@ for((i = 10; i<= $max_distance; i = i+10)) do
 done
 
 output_dir=$test_dir/serial
-for((i = 10; i<= $max_distance; i = i+10)) do
-   echo "Running $i miles of $max_distances in OpenCL"
+for((i = 1; i<= $max_distance; i = i+1)) do
+   echo "Running $i miles of $max_distance in OpenCL"
    echo "Run with $i points:" > $output_dir/time_$i.out
    (time ../splat-1.4.0/splat-hd -d ../../splat/eledata/sdf/ -L 10 -R $i -t W6BHZ.qth) 1>$output_dir/stdout_$i.out 2>> $output_dir/time_$i.out
    echo "Run with $i miles:" > $output_dir/gprof_$i.out
