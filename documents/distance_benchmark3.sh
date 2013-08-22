8b7ba08f200f6d9ecd720dc98c60ce043561e2d3
Andrew J. Musselman <ki6uoc@gmail.com>
2013-08-22 13:16:13 -0700

    Updated scripts to make them easier to use

--- documents/distance_benchmark3.sh
+++ documents/distance_benchmark3.sh
@@ -38,20 +38,22 @@ output_dir=$test_dir/opencl
 run_dir=../psplat/
 for((i = 1; i<= $max_distance; i = i+1)) do
    cd $run_dir
-   echo "Running $i miles of $max_distance in OpenCL"
-   echo "Run with $i points:" > $output_dir/time_$i.out
-   (time ../psplat/psplat -d ../../splat/eledata/sdf/ -L 10 -R $i -t ../documents/W6BHZ.qth) 1>$output_dir/stdout_$i.out 2>> $output_dir/time_$i.out
-   echo "Run with $i miles:" > $output_dir/gprof_$i.out
-   gprof ../psplat/psplat >> $output_dir/gprof_$i.out
+   run_num=`printf %03d $i`
+   echo "Running $run_num miles of $max_distance in OpenCL"
+   echo "Run with $run_num points:" > $output_dir/time_$run_num.out
+   (time ../psplat/psplat -d ../../splat/eledata/sdf/ -L 10 -R $run_num -t ../documents/W6BHZ.qth) 1>$output_dir/stdout_$run_num.out 2>> $output_dir/time_$run_num.out
+   echo "Run with $run_num miles:" > $output_dir/gprof_$run_num.out
+   gprof ../psplat/psplat >> $output_dir/gprof_$run_num.out
 done
 
 output_dir=$test_dir/serial
 run_dir=../splat-1.4.0/
 for((i = 1; i<= $max_distance; i = i+1)) do
    cd $run_dir
-   echo "Running $i miles of $max_distance in Serial"
-   echo "Run with $i points:" > $output_dir/time_$i.out
-   (time ../splat-1.4.0/splat-hd -d ../../splat/eledata/sdf/ -L 10 -R $i -t ../documents/W6BHZ.qth) 1>$output_dir/stdout_$i.out 2>> $output_dir/time_$i.out
-   echo "Run with $i miles:" > $output_dir/gprof_$i.out
-   gprof ../splat-1.4.0/splat-hd >> $output_dir/gprof_$i.out
+   run_num=`printf %03d $i`
+   echo "Running $run_num miles of $max_distance in Serial"
+   echo "Run with $run_num points:" > $output_dir/time_$run_num.out
+   (time ../splat-1.4.0/splat-hd -d ../../splat/eledata/sdf/ -L 10 -R $run_num -t ../documents/W6BHZ.qth) 1>$output_dir/stdout_$run_num.out 2>> $output_dir/time_$run_num.out
+   echo "Run with $run_num miles:" > $output_dir/gprof_$run_num.out
+   gprof ../splat-1.4.0/splat-hd >> $output_dir/gprof_$run_num.out
 done
